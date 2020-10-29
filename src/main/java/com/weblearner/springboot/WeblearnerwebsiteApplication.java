c0f2af62e684707c5b36b3841774ed7a0686711f
Raghu Seetharaman <ramyaraghu2510@gmail.com>
2020-10-28 20:32:33 -0400

    first commit

---
+++ src/main/java/com/weblearner/springboot/WeblearnerwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.weblearner.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblearnerwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblearnerwebsiteApplication.class, args);
+	}
+
+}
